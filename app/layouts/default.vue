648eb91e6c2fa7ab68226b9fc8ca953ce7faf3f2
Alfredo Marvulle <alfredo.marvulle@gmail.com>
2026-02-17 21:45:37 -0300

    style: add tailwind and style header

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <ul>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink to="/" class="font-bold">Logo</NuxtLink>
+        <ul class="flex gap-4">
           <li><NuxtLink to="/">Home</NuxtLink></li>
           <li><NuxtLink to="/about">About</NuxtLink></li>
-          <li><NuxtLink to="/products">Products</NuxtLink></li>
+          <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
         </ul>
       </nav>
     </header>
-
     <!-- ? PAGE CONTENT -->
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
     <!-- ? END PAGE CONTENT -->
--- app/layouts/products.vue
+++ app/layouts/products.vue
@@ -1,26 +1,29 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <ul>
-          <li><NuxtLink to="/">Home</NuxtLink></li>
-        </ul>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4">
+        <NuxtLink to="/products" class="font-bold">Logo</NuxtLink>
       </nav>
     </header>
 
-    <!-- ? START PAGE CONTENT -->
-    <div>
+    <!-- ? PAGE CONTENT -->
+    <div class="container mx-auto p-4">
       <slot />
     </div>
     <!-- ? END PAGE CONTENT -->
 
-    <header>
-      <nav>
-        <ul>
-          <li><NuxtLink to="/about">About</NuxtLink></li>
-          <li><NuxtLink to="/products">Products</NuxtLink></li>
-        </ul>
-      </nav>
-    </header>
+    <footer class="container mx-auto p-4 flex justify-between border-t-2">
+      <ul class="flex gap-4">
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/products">Products</NuxtLink></li>
+      </ul>
+    </footer>
   </div>
 </template>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
